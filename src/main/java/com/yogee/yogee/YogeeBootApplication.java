2f65349058e06141d94b1f1e914dc6131b68bbb6
jackqth <283324591@qq.com>
2018-03-21 18:23:21 +0800

    ecache 等集成

--- src/main/java/com/yogee/yogee/YogeeBootApplication.java
+++ src/main/java/com/yogee/yogee/YogeeBootApplication.java
@@ -2,13 +2,16 @@ package com.yogee.yogee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
- * Demo class
+ * Yogee2——Power By Yogee
  *
  * @author Jackqth
  * @date 2018/3/20
  */
 @SpringBootApplication
+@EnableScheduling
 public class YogeeBootApplication {
 
 	public static void main(String[] args) {
