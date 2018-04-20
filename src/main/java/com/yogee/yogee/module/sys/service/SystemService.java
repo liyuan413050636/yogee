445b509c97a1ef7c25c0c9221e624839e85c52d0
413050636@qq.com <liyuan123>
2018-04-20 15:30:19 +0800

    完善sys

--- src/main/java/com/yogee/yogee/module/sys/service/SystemService.java
+++ src/main/java/com/yogee/yogee/module/sys/service/SystemService.java
@@ -400,7 +400,7 @@ public class SystemService extends BaseService {
 	public static boolean printKeyLoadMessage() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("\r\n======================================================================\r\n");
-		sb.append("\r\n    欢迎使用 " + Global.getConfig("productName") + "  - Powered By http://jeesite.com\r\n");
+		sb.append("\r\n    欢迎使用 " + Global.getConfig("productName") + "  - Powered By Yogee\r\n");
 		sb.append("\r\n======================================================================\r\n");
 		System.out.println(sb.toString());
 		return true;
